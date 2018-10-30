5ea7f7687beb2f18d8ad1cb03bd99c66a230ef06
ganghee <idiosauri@naver.com>
2018-10-30 11:07:45 +0900

    상수변수

---
+++ Android/Constant.java
@@ -0,0 +1,10 @@
+package com.mobitant.bookapp;
+
+public interface Constant {
+    int MAX_LENGTH_DESCRIPTION = 50;
+
+
+    String ORDER_TYPE_KIND = "";
+    String ORDER_TYPE_FAVORITE = "keep_cnt";
+    String ORDER_TYPE_RECENT = "reg_date";
+}
